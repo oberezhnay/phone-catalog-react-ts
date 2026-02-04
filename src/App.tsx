9f238709d9a51afdeb7c3ba615eae374ca2dd770
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2026-02-04 15:14:38 +0200

    added files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import React from 'react';
+import { Outlet } from 'react-router-dom';
 import './App.scss';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+export const App = () => (
+  <div className="AppWrapper">
+    <Header />
+    <div className="App">
+      <Outlet />
     </div>
-  );
-};
+    <Footer />
+  </div>
+);
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,50 @@
+import { NavLink } from 'react-router-dom';
+import styles from './Footer.module.scss';
+
+export const Footer = () => (
+  <div>
+    <footer className={styles.footer}>
+      <NavLink to="/" className={styles.logo}>
+        <img src="img/logo/Logo.svg" alt="Nice gadgets logo" />
+      </NavLink>
+
+      <ul className={styles['footer-list']}>
+        <li className={styles['footer-item']}>
+          <a
+            href="https://github.com/oberezhnay/react_phone-catalog"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </li>
+        <li className={styles['footer-item']}>
+          <a
+            href="https://github.com/oberezhnay/react_phone-catalog"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Contacts
+          </a>
+        </li>
+        <li className={styles['footer-item']}>
+          <a
+            /* eslint-disable-next-line max-len */
+            href="https://docs.github.com/ru/site-policy/github-terms/github-terms-of-service"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rights
+          </a>
+        </li>
+      </ul>
+
+      <div className={styles['back-to-top']}>
+        <p className={styles.text}>Back to top</p>
+        <button onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
+          <img src="img/icons/ChevronArrowUp.svg" alt="Back to top" />
+        </button>
+      </div>
+    </footer>
+  </div>
+);
